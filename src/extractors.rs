a89e52baa6a6b6d76331ae8fd10bf2208108733e
Shresth Prasad <shresthprasad7@gmail.com>
2025-08-05 02:45:02 +0530

    refactor: Create separate function to extract data from steam app

--- src/extractors.rs
+++ src/extractors.rs
@@ -15,11 +15,22 @@ pub fn extract_game_id(exec_field: &str) -> Option<&str> {
 pub fn extract_icon_id(game_id: &str, is_dummy: bool) -> anyhow::Result<String> {
     let icon_id_regex: Regex = Regex::new(r#""clienticon"\s+"([^"]+)""#).unwrap();
 
+    // Return a dummy string for testing
     if is_dummy {
-        // Return a dummy string for testing
         return Ok("9102f4v4h3491h8hf4c1u2394n184n1th4".into());
     }
 
+    let cmd_output = get_steam_app_data(game_id)?;
+
+    if let Some(capture) = icon_id_regex.captures(&cmd_output) {
+        Ok(capture[1].to_owned())
+    } else {
+        bail!("No icon id found! Something has gone wrong...");
+    }
+}
+
+#[inline]
+fn get_steam_app_data(game_id: &str) -> anyhow::Result<String> {
     let cmd = Command::new("steamcmd")
         .arg("+login")
         .arg("anonymous")
@@ -33,12 +44,8 @@ pub fn extract_icon_id(game_id: &str, is_dummy: bool) -> anyhow::Result<String> 
         bail!("steamcmd failed with status: {}", cmd.status);
     }
 
-    let cmd_output = std::str::from_utf8(&cmd.stdout)
-        .context("`steamcmd` output is not valid UTF-8")?;
+    let cmd_output =
+        std::str::from_utf8(&cmd.stdout).context("`steamcmd` output is not valid UTF-8")?;
 
-    if let Some(capture) = icon_id_regex.captures(&cmd_output) {
-        Ok(capture[1].to_owned())
-    } else {
-        bail!("No icon id found! Something has gone wrong...");
-    }
+    Ok(cmd_output.to_owned())
 }
